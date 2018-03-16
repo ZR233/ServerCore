fe828d09149107bda6d35c87c2536652e4d2f3cb
rui zhou <zrufo747@outlook.com>
2018-03-16 19:20:16 +0800

    11

---
+++ ServerCore/Server.h
@@ -0,0 +1,12 @@
+#pragma once
+class Server
+{
+public:
+	Server();
+	~Server();
+	int add(int a, int b)
+	{
+		return a + b;
+	}
+};
+
---
+++ UnitTest1/unittest1.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../ServerCore/Server.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: 在此输入测试代码
+			Server s;
+			auto a = s.add(1, 2);
+			Assert::AreEqual(a, 3);
+		}
+
+	};
+}
