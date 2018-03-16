6db299b27bfef4d4c0bb73fcf7da897cbe65b265
rui zhou <zrufo747@outlook.com>
2018-03-16 20:08:02 +0800

    111

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "../ServerCore/Server.h"
-
+#include "../../ZRUtils/ZRUtils/Utils.hpp"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest1
@@ -17,6 +17,11 @@ namespace UnitTest1
 			auto a = s.add(1, 2);
 			Assert::AreEqual(a, 3);
 		}
-
+		TEST_METHOD(ntoh64)
+		{
+			// TODO: 在此输入测试代码
+			uint64_t a = 111;
+			Assert::AreEqual(ntohll(a), zrutils::ntoh64(a));
+		}
 	};
 }
